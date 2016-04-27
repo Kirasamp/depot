97ed84a7ad1a1b1dcee2e8dce75875845db1e0c6
Kim <kirasamp@gmail.com>
2016-04-27 16:10:33 +0200

    returned cart to top of sidebar

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -65,8 +65,7 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart = current_cart
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
 
     respond_to do |format|
