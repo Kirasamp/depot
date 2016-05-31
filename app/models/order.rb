9719e973a716c8738c17270233928d50d4f093aa
Kim <kirasamp@gmail.com>
2016-05-31 20:54:28 +0200

    Added associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ActiveRecord::Base
+	has_many :line_items, dependent: :destroy
+
 	PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
+	
 	validates :name, :email, :address, presence: true
 	validates :pay_type, inclusion: PAYMENT_TYPES
 end
